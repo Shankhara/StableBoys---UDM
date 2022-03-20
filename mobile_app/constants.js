94b68f3ca6d1113ba4b049d9d4a4769ab72ce7a7
Shankhara <boulenge.charles28@gmail.com>
2022-03-20 15:57:49 +0100

    add demo video

--- mobile_app/constants.js
+++ mobile_app/constants.js
@@ -2,6 +2,7 @@ import { Dimensions } from 'react-native'
 
 export const API_KEY = 'pznNDBukYDS0nutULkGBxjLi89VFMAKM'
 export const SB_WALLET = '0xa86458DC41B35b0c29dBB26C887D3359c40c3459'
+// export const SB_WALLET = '0xbD25280d802EB1a71aA66bD7827603503F755c7d'
 // export const SERVER_URL = 'http://192.168.1.16:3000'
 export const SERVER_URL = 'http://2d3f-2a01-cb00-c27-3f00-5106-f5bd-fa98-d55b.ngrok.io'
 export const WIDTH = Dimensions.get('window').width
